8601b38a82867ff7d49783b7d65cf11fa1337e06
lllii-tools <llliitools20@gmail.com>
2020-01-30 01:51:14 +0900

    uikit スピンボタン実装完了　他

--- uikit/uikit.js
+++ uikit/uikit.js
@@ -1,90 +1,59 @@
-const inputs = document.getElementsByTagName('input');
-for (let i = 0; i < inputs.length; i++) {
-    const names = inputs[i].getAttribute('name');
-    //console.log(names);
+//
+// フォーカス時全選択
+//
+const selectAll = document.getElementsByClassName('selectAll');
+for (let i = 0; i < selectAll.length; i++) {
+    selectAll[i].addEventListener('focus', () => selectAll[i].select());
 }
-inputs[6].addEventListener('click', function () {
-    //console.log('input clicked!');
-});
-const testClass = document.getElementsByClassName('test');
-const plusBtn = document.getElementsByClassName('plus');
-const minusBtn = document.getElementsByClassName('minus');
-const spinBtn = document.getElementsByClassName('spinBtn');
-// クリック時
-function plus() {
-    testClass[0].value = (testClass[0].value == '') ? 1 :
-        Number(testClass[0].value) + Number(plusBtn[0].value);
-}
-function minus() {
-    testClass[0].value = (testClass[0].value == '0') ? 0 :
-        Number(testClass[0].value) - Number(minusBtn[0].value);
-}
-plusBtn[0].addEventListener('click', plus);
-minusBtn[0].addEventListener('click', minus);
-// 長押し時
-let timer;
-let delay;
-function start(x) {
-    function count() {
-        timer = setInterval(x, 60);
+//
+// スピンボタン
+//
+const spinInput = document.getElementsByClassName('spin-input');
+for (let i = 0; i < spinInput.length; i++) {
+    const spinInputId = spinInput[i].getAttribute('id');
+    const spinInputIdEle = document.getElementById(spinInputId);
+    const spinPlus = document.getElementsByClassName('spin-plus');
+    const spinPlusId = spinInputId + 'Plus';
+    spinPlus[i].setAttribute('id', spinPlusId);
+    const plusBtn = document.getElementById(spinPlusId);
+    const plusBtnVal = plusBtn.getAttribute('value');
+    const spinMinus = document.getElementsByClassName('spin-minus');
+    const spinMinusId = spinInputId + 'Minus';
+    spinMinus[i].setAttribute('id', spinMinusId);
+    const minusBtn = document.getElementById(spinMinusId);
+    const minusBtnVal = minusBtn.getAttribute('value');
+    function plus() {
+        spinInputIdEle.value = (spinInputIdEle.value == '') ? 1 :
+            Number(spinInputIdEle.value) + Number(plusBtnVal);
     }
-    delay = setTimeout(count, 600);
-}
-function end() {
-    clearTimeout(delay);
-    clearInterval(timer);
-}
-// クリック
-plusBtn[0].addEventListener('mousedown', () => start(plus));
-plusBtn[0].addEventListener('mouseup', end);
-plusBtn[0].addEventListener('mouseout', end);
-minusBtn[0].addEventListener('mousedown', () => start(minus));
-minusBtn[0].addEventListener('mouseup', end);
-minusBtn[0].addEventListener('mouseout', end);
-// タップ
-plusBtn[0].addEventListener('touchstart', () => start(plus));
-plusBtn[0].addEventListener('touchend', end);
-minusBtn[0].addEventListener('touchstart', () => start(minus));
-minusBtn[0].addEventListener('touchend', end);
-function numberInput() {
-    const target = document.getElementsByClassName('number-input');
-    function targets() {
-        for (let i = 0; i < target.length; i++) {
-            const input = target[i].getElementsByTagName('input');
-            const name = input[0].getAttribute('name');
-            function calc() {
-                const inputValue = Number(input[0].value);
-                const plus = document.getElementsByClassName('number-input-plus');
-                const plusVal = plus[0].value;
-                const minus = document.getElementsByClassName('number-input-minus');
-                const minusVal = minus[0].value;
-                //if(){
-                plus[0].addEventListener('click', function () {
-                    i++;
-                    const sum = inputValue + 1;
-                    input[0].value = String(sum);
-                    console.log('プラス' + sum);
-                });
-                minus[0].addEventListener('click', function () {
-                    i++;
-                    const sum = inputValue - i;
-                    input[0].value = String(sum);
-                    console.log('マイナス' + sum);
-                });
-                //}
-                console.log(plus[0]);
-            }
-            calc();
-            console.log(name);
+    function minus() {
+        spinInputIdEle.value = (spinInputIdEle.value == 0) ? 0 :
+            Number(spinInputIdEle.value) - Number(minusBtnVal);
+    }
+    let timer;
+    let delay;
+    function start(x) {
+        function count() {
+            timer = setInterval(x, 60);
         }
+        delay = setTimeout(count, 600);
     }
-    document.addEventListener('change', targets);
-    /*
-    const button = document.getElementsByTagName('button');
-    for(let i=0; i<button.length; i++){
-      button[i].addEventListener('click', calc);
+    function end() {
+        clearTimeout(delay);
+        clearInterval(timer);
     }
-    */
-    console.log('Hello');
+    // クリック
+    plusBtn.addEventListener('click', plus);
+    plusBtn.addEventListener('mousedown', () => start(plus));
+    plusBtn.addEventListener('mouseup', end);
+    plusBtn.addEventListener('mouseout', end);
+    minusBtn.addEventListener('click', minus);
+    minusBtn.addEventListener('mousedown', () => start(minus));
+    minusBtn.addEventListener('mouseup', end);
+    minusBtn.addEventListener('mouseout', end);
+    // タップ
+    plusBtn.addEventListener('touchstart', () => start(plus));
+    plusBtn.addEventListener('touchend', end);
+    minusBtn.addEventListener('touchstart', () => start(minus));
+    minusBtn.addEventListener('touchend', end);
 }
-numberInput();
--- uikit/uikit.ts
+++ uikit/uikit.ts
@@ -1,97 +1,65 @@
-const inputs = document.getElementsByTagName('input') as HTMLCollectionOf<HTMLElement>;
-for(let i=0; i<inputs.length; i++){
-  const names = inputs[i].getAttribute('name');
-  //console.log(names);
+//
+// フォーカス時全選択
+//
+const selectAll = document.getElementsByClassName('selectAll');
+for(let i=0; i<selectAll.length; i++){
+  selectAll[i].addEventListener('focus',() => (selectAll[i] as HTMLInputElement).select())
 }
-inputs[6].addEventListener('click', function(){
-  //console.log('input clicked!');
-})
 
 
-const testClass = document.getElementsByClassName('test') as HTMLCollectionOf<HTMLFormElement>;
-const plusBtn = document.getElementsByClassName('plus') as HTMLCollectionOf<HTMLFormElement>;
-const minusBtn = document.getElementsByClassName('minus') as HTMLCollectionOf<HTMLFormElement>;
-const spinBtn = document.getElementsByClassName('spinBtn') as HTMLCollectionOf<HTMLFormElement>;
-// クリック時
-function plus(){
-  testClass[0].value = (testClass[0].value == '') ? 1:
-  Number(testClass[0].value) + Number(plusBtn[0].value);
-}
-function minus(){
-  testClass[0].value = (testClass[0].value == '0') ? 0:
-  Number(testClass[0].value) - Number(minusBtn[0].value);
-}
-plusBtn[0].addEventListener('click',plus);
-minusBtn[0].addEventListener('click',minus);
+//
+// スピンボタン
+//
+const spinInput = document.getElementsByClassName('spin-input') as HTMLCollectionOf<HTMLElement>;
+for(let i=0; i<spinInput.length; i++){
+  const spinInputId = spinInput[i].getAttribute('id');
+  const spinInputIdEle:any = <HTMLInputElement>document.getElementById(spinInputId);
 
-// 長押し時
-let timer;
-let delay;
-function start(x:any){
-  function count(){
-    timer = setInterval(x, 60);
+  const spinPlus = document.getElementsByClassName('spin-plus') as HTMLCollectionOf<HTMLFormElement>;
+  const spinPlusId = spinInputId + 'Plus';
+  spinPlus[i].setAttribute('id',spinPlusId);
+  const plusBtn = document.getElementById(spinPlusId);
+  const plusBtnVal = plusBtn.getAttribute('value');
+  const spinMinus = document.getElementsByClassName('spin-minus') as HTMLCollectionOf<HTMLFormElement>;
+  const spinMinusId = spinInputId + 'Minus';
+  spinMinus[i].setAttribute('id',spinMinusId);
+  const minusBtn = document.getElementById(spinMinusId);
+  const minusBtnVal = minusBtn.getAttribute('value');
+
+  function plus(){
+    spinInputIdEle.value = (spinInputIdEle.value == '') ? 1:
+    Number(spinInputIdEle.value) + Number(plusBtnVal);
   }
-  delay = setTimeout(count,600);
-}
-function end(){
-  clearTimeout(delay);
-  clearInterval(timer);
-}
-// クリック
-plusBtn[0].addEventListener('mousedown',() => start(plus))
-plusBtn[0].addEventListener('mouseup',end)
-plusBtn[0].addEventListener('mouseout',end)
-minusBtn[0].addEventListener('mousedown',() => start(minus))
-minusBtn[0].addEventListener('mouseup',end)
-minusBtn[0].addEventListener('mouseout',end)
-// タップ
-plusBtn[0].addEventListener('touchstart',() => start(plus))
-plusBtn[0].addEventListener('touchend',end)
-minusBtn[0].addEventListener('touchstart',() => start(minus))
-minusBtn[0].addEventListener('touchend',end)
+  function minus(){
+    spinInputIdEle.value = (spinInputIdEle.value == 0) ? 0:
+    Number(spinInputIdEle.value) - Number(minusBtnVal);
+  }
 
-
-
-function numberInput(){
-  const target = document.getElementsByClassName('number-input') as HTMLCollectionOf<HTMLElement>;
-  function targets(){
-    for(let i=0; i<target.length; i++){
-      const input = target[i].getElementsByTagName('input');
-      const name = input[0].getAttribute('name');
-      function calc(){
-        const inputValue = Number(input[0].value);
-        const plus = document.getElementsByClassName('number-input-plus');
-        const plusVal = (plus[0] as HTMLFormElement).value;
-        const minus = document.getElementsByClassName('number-input-minus');
-        const minusVal = (minus[0] as HTMLFormElement).value;
-        //if(){
-          plus[0].addEventListener('click',function(){
-            i++;
-            const sum = inputValue + 1;
-            input[0].value = String(sum);
-            console.log('プラス' + sum);
-          });
-          minus[0].addEventListener('click',function(){
-            i++;
-            const sum = inputValue - i;
-            input[0].value = String(sum);
-            console.log('マイナス' + sum);
-          });
-        //}
-        console.log(plus[0]);
-      }
-      calc();
-      console.log(name);
+  let timer;
+  let delay;
+  function start(x:any){
+    function count(){
+      timer = setInterval(x, 60);
     }
+    delay = setTimeout(count,600);
   }
-  document.addEventListener('change',targets);
-  /*
-  const button = document.getElementsByTagName('button');
-  for(let i=0; i<button.length; i++){
-    button[i].addEventListener('click', calc);
+  function end(){
+    clearTimeout(delay);
+    clearInterval(timer);
   }
-  */
 
-  console.log('Hello');
+  // クリック
+  plusBtn.addEventListener('click',plus)
+  plusBtn.addEventListener('mousedown',() => start(plus))
+  plusBtn.addEventListener('mouseup',end)
+  plusBtn.addEventListener('mouseout',end)
+  minusBtn.addEventListener('click',minus)
+  minusBtn.addEventListener('mousedown',() => start(minus))
+  minusBtn.addEventListener('mouseup',end)
+  minusBtn.addEventListener('mouseout',end)
+  // タップ
+  plusBtn.addEventListener('touchstart',() => start(plus))
+  plusBtn.addEventListener('touchend',end)
+  minusBtn.addEventListener('touchstart',() => start(minus))
+  minusBtn.addEventListener('touchend',end)
 }
-numberInput();
